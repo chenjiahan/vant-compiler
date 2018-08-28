9025ce5770c234ca9942d7b50581ee10885a698d
陈嘉涵 <chenjiahan@youzan.com>
2018-08-28 17:11:43 +0800

    [breaking change] upgrade to babel 7

--- src/babel.js
+++ src/babel.js
@@ -1,9 +1,8 @@
-import { transform } from 'babel-core';
+import { transform } from '@babel/core';
 import path from 'path';
 
 const defaultConfig = {
-  babelrc: true,
-  extends: path.resolve(__dirname, '../.babelrc')
+  configFile: path.join(__dirname, '../babel.config.js')
 };
 
 export default function(source, config = {}) {
